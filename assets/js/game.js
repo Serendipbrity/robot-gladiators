b91c8d404aa855f75720f3538a5c59c96a235ab7
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-15 13:45:52 -0400

    number options added to shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -143,19 +143,17 @@ if (playAgainConfirm) {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to amek a choice."
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE"
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt){
-        case "REFILL":
-        case "refill": 
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             
             // do nothing so function will end
@@ -204,7 +202,7 @@ var playerInfo = {
         this.money -=7;
     }
     else{
-        window.alert("You don't have enought money!");
+        window.alert("You don't have enough money!");
     }
   }
 };
