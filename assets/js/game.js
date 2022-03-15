ef83432ab9a339ace0f05ef0df7410cc4fa74b3b
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-15 13:04:19 -0400

    fight or skip works with upper or lowercase

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,32 +4,39 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var fightOrSkip = function(){
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose.");    
 
-
-// fight function with parameter for enemys name
-var fight = function(enemy) {
-    
-  while(playerInfo.health > 0 && enemy.health > 0){
-
-    // ask player if theyd like to fight or skip
-
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose.");    
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.")
+        return fightOrSkip();
+    }
+    promptFight = promptFight.toLowerCase();
 
         // if player picks "skip" confirm and then stop the loop
-        if (promptFight === "skip" || promptFight === "SKIP") {
+        if (promptFight === "skip") {
         // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-       
+
         // if yes (true) leave fight
         if (confirmSkip){ 
             window.alert(playerInfo.name + " has chosen to skip the fight. Goodbye!");
         //subtract money from playerMoney for skipping
             playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerInfo.money", playerInfo.money)
-            break;
+           return true;
         }
     }
-       
+    return false;
+};
+
+// fight function with parameter for enemys name
+var fight = function(enemy) {
+    
+  while(playerInfo.health > 0 && enemy.health > 0){
+       if (fightOrSkip()){
+           break;
+       }
+    
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     enemy.health = Math.max(0, enemy.health - damage);
         // Log a resulting message to the console so we know that it worked
